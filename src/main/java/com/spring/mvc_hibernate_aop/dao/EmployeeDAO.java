c9df54da6140d6659a8fee9898b4fd1b4652fa2a
v2nes <tischenko_ivan2@mail.ru>
2023-11-09 21:44:31 +0300

    Initial commit

---
+++ src/main/java/com/spring/mvc_hibernate_aop/dao/EmployeeDAO.java
@@ -0,0 +1,16 @@
+package com.spring.mvc_hibernate_aop.dao;
+
+import com.spring.mvc_hibernate_aop.entity.Employee;
+
+import java.util.List;
+
+public interface EmployeeDAO {
+    List<Employee> getAllEmployees();
+
+    void saveEmployee(Employee employee);
+
+    Employee getEmployee(int id);
+
+    void deleteEmployee(int id);
+
+}
